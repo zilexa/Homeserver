42ceb2497068eba71700b04071ac015a32e4ae87
ZileXa <zilexa@gmx.com>
2020-05-12 23:01:06 +0200

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,15 +1,18 @@
 ## Required actions before running docker-compose.yml
 #!/bin/bash
 # Create PiHole log file
+mkdir -p ${USERDIR}/docker/pihole/var-log
 touch ${USERDIR}/docker/pihole/var-log/pihole.log
 
 # Create Traefik files
+mkdir -p ${USERDIR}/docker/traefik
 touch $USERDIR/docker/traefik/traefik.log
 touch ${USERDIR}/docker/traefik/acme.json
 chmod 600 $USERDIR/docker/traefik/acme.json
 wget -P $USERDIR/docker/traefik https://raw.githubusercontent.com/zilexa/Mediaserver/master/traefik/traefik.toml
 
 # Create Firefox-Syncserver file & generate secret
+mkdir -p ${USERDIR}/docker/firefox-syncserver/secret
 touch $USERDIR/docker/firefox-syncserver/secret/secret.txt
 sed -i '1s/^/[syncserver]\n/' $USERDIR/docker/firefox-syncserver/secret/secret.txt
 head -c 20 /dev/urandom | sha1sum >> $USERDIR/docker/firefox-syncserver/secret/secret.txt
