884fc7d758251f2f0c1b951df07626baecfe2bd8
ZileXa <zilexa@gmx.com>
2020-05-12 22:46:39 +0200

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -8,4 +8,8 @@ touch $USERDIR/docker/traefik/traefik.log
 touch ${USERDIR}/docker/traefik/acme.json
 chmod 600 $USERDIR/docker/traefik/acme.json
 wget blabla
-
+#
+# Create Firefox-Syncserver file & generate secret
+touch $USERDIR/docker/firefox-syncserver/secret/secret.txt
+sed -i '1s/^/[syncserver]\n/' $USERDIR/docker/firefox-syncserver/secret/secret.txt
+head -c 20 /dev/urandom | sha1sum >> $USERDIR/docker/firefox-syncserver/secret/secret.txt
