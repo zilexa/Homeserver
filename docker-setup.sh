987e92e97d1401dd3b59c0fe55c8a0f83c47bb5b
ZileXa <zilexa@gmx.com>
2020-05-13 12:41:03 +0200

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -7,6 +7,7 @@ touch ${USERDIR}/docker/pihole/var-log/pihole.log
 # Create Traefik files
 mkdir -p ${USERDIR}/docker/traefik
 touch $USERDIR/docker/traefik/traefik.log
+chmod 600 $USERDIR/docker/traefik/traefik.log
 touch ${USERDIR}/docker/traefik/acme.json
 chmod 600 $USERDIR/docker/traefik/acme.json
 wget -P $USERDIR/docker/traefik https://raw.githubusercontent.com/zilexa/Mediaserver/master/traefik/traefik.toml
@@ -14,5 +15,5 @@ wget -P $USERDIR/docker/traefik https://raw.githubusercontent.com/zilexa/Mediase
 # Create Firefox-Syncserver file & generate secret
 mkdir -p ${USERDIR}/docker/firefox-syncserver/secret
 touch $USERDIR/docker/firefox-syncserver/secret/secret.txt
-echo "[syncserver]" >> $USERDIR/docker/firefox-syncserver/secret/secret.txt
+# echo "[syncserver]" >> $USERDIR/docker/firefox-syncserver/secret/secret.txt # file should only contain secret contrary to mozilla doc
 head -c 20 /dev/urandom | sha1sum | awk '{print $1}' >> $USERDIR/docker/firefox-syncserver/secret/secret.txt
