005d13a2874eaf9760a946033b74e7f014973f5c
ZileXa <zilexa@gmx.com>
2020-06-10 11:34:16 +0200

    Update prep-docker.sh

--- prep-docker.sh
+++ prep-docker.sh
@@ -1,5 +1,6 @@
 ## Required actions before running docker-compose.yml
 # See https://github.com/zilexa/Homeserver
+# Run this script with sudo -i to inherit the correct env variables ($HOME should be home/user instead of home/root)
 #!/bin/bash
 #Set environment variables to be used by Docker (i.e. requires TZ in quotes)
 wget -P $HOME/docker https://raw.githubusercontent.com/zilexa/Homeserver/master/.env
