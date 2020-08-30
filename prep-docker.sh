c0a6ebdc9c618e21a96be728e7da9b5c46b5099f
ZileXa <zilexa@gmx.com>
2020-08-30 14:18:52 +0200

    Update prep-docker.sh

--- prep-docker.sh
+++ prep-docker.sh
@@ -3,6 +3,18 @@
 # See https://github.com/zilexa/Homeserver
 # Run this script with sudo -E to make sure $HOME points to /home/username instead of /root
 #
+# Install Docker, Docker-Compose and bash completion for Compose
+wget -qO - https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt -y update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.26.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+# Make docker-compose file an executable file and add the current user to the docker container
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker ${USER}
+
 # Create the docker folder
 mkdir -p $HOME/docker
 # Get environment variables to be used by Docker (i.e. requires TZ in quotes)
