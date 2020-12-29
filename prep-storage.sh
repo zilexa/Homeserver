539f0b9320a1ed8e04b543f41649ea3d6fddc11e
ZileXa <zilexa@gmx.com>
2020-12-29 14:23:25 +0100

    Update prep-storage.sh

--- prep-storage.sh
+++ prep-storage.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # Storage setup
 # install MergerFS
 sudo apt -y install mergerfs
-wget https://github.com/trapexit/mergerfs/releases/download/2.32.2/mergerfs_2.32.2.ubuntu-focal_arm64.deb
+wget https://github.com/trapexit/mergerfs/releases/download/2.32.2/mergerfs_2.32.2.ubuntu-focal_amd64.deb
 sudo apt -y install ./mergerfs*.deb
 rm mergerfs*.deb
 
@@ -14,14 +15,13 @@ cd snapraid-11.5/
 make
 make check
 make install
-cd ..
-sudo cp ~/snapraid-11.5/snapraid.conf.example /etc/snapraid.conf.example
-cd ..
+cd $HOME/Downloads
+sudo cp snapraid-11.5/snapraid.conf.example /etc/snapraid.conf.example
 rm -rf snapraid*
 # Get drive IDs
 #ls -la /dev/disk/by-id/ | grep part1  | cut -d " " -f 11-20
 # get SnapRAID config
-sudo wget -P /etc https://raw.githubusercontent.com/zilexa/Mediaserver/master/snapraid.conf
+sudo wget -P /etc https://raw.githubusercontent.com/zilexa/Homeserver/master/snapraid/snapraid.conf
 # SnapRAID create path for local content file
 sudo mkdir -p /var/snapraid/
 
@@ -35,5 +35,5 @@ sudo mkdir -p /mnt/disks/{data_Cache,data_TopLeft,data_TopRight,parity_BottomLef
 
 
 # Configure etc/fstab FIRST (open new terminal for the user, pause the current)
+read -p "Use the example etc/fstab to add your disk mounts and MergerFS mounts"
 x-terminal-emulator -e sudo nano /etc/fstab
-read -p "Add the required content to etc/fstab in a seperate terminal BEFORE continuing, press enter to continue"
