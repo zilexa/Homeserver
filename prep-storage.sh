7fb0e5ef9e1fefb9ad2c2bf1f2a104e2e9dceede
ZileXa <zilexa@gmx.com>
2020-12-29 13:59:24 +0100

    Update prep-storage.sh

--- prep-storage.sh
+++ prep-storage.sh
@@ -1,20 +1,20 @@
 # Storage setup
 # install MergerFS
-wget https://github.com/trapexit/mergerfs/releases/download/2.30.0/mergerfs_2.30.0.ubuntu-xenial_amd64.deb
-sudo apt -y install ./mergerfs_2.30.0.ubuntu-xenial_amd64.deb
+wget https://github.com/trapexit/mergerfs/releases/download/2.32.2/mergerfs_2.32.2.ubuntu-focal_arm64.deb
+sudo apt -y install ./mergerfs*.deb
 rm mergerfs*.deb
 
 # install SnapRAID
 cd
-wget https://github.com/amadvance/snapraid/releases/download/v11.3/snapraid-11.3.tar.gz
-tar xzvf snapraid-11.3.tar.gz
-cd snapraid-11.3/
+wget https://github.com/amadvance/snapraid/releases/download/v11.5/snapraid-11.5.tar.gz
+tar xzvf snapraid*.tar.gz
+cd snapraid-11.5/
 ./configure
 make
 make check
 make install
 cd ..
-cp ~/snapraid-11.3/snapraid.conf.example /etc/snapraid.conf
+cp ~/snapraid-11.5/snapraid.conf.example /etc/snapraid.conf
 cd ..
 rm -rf snapraid*
 # Get drive IDs
@@ -27,12 +27,11 @@ mkdir -p /var/snapraid/
 # install nocache - required to move files from pool to archive with rsync
 sudo apt -y install nocache
 
-# Create the required folders for MergerFS pool
-mkdir -p /mnt/archive
+# Create the required folders to mount the disks and MergerFS pools
 mkdir -p /mnt/pool
-mkdir -p /mnt/data/cache
-mkdir -p /mnt/data/{disk1,disk2}
-mkdir -p /mnt/parity/pardisk1
+mkdir -p /mnt/pool-archive
+mkdir -p /mnt/disks/{data_Cache,data_TopLeft,data_TopRight,parity_BottomLeft,parity_BottomRight}
+
 
 # Configure etc/fstab FIRST (open new terminal for the user, pause the current)
 x-terminal-emulator -e sudo nano /etc/fstab
