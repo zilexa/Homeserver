3c3ca02a78b008e576bcbe309dd3fe39eb8d4186
ZileXa <zilexa@gmx.com>
2020-12-29 14:08:54 +0100

    Update prep-storage.sh

--- prep-storage.sh
+++ prep-storage.sh
@@ -1,11 +1,12 @@
 # Storage setup
 # install MergerFS
+sudo apt -y install mergerfs
 wget https://github.com/trapexit/mergerfs/releases/download/2.32.2/mergerfs_2.32.2.ubuntu-focal_arm64.deb
 sudo apt -y install ./mergerfs*.deb
 rm mergerfs*.deb
 
 # install SnapRAID
-cd
+sudo apt -y install gcc git make
 wget https://github.com/amadvance/snapraid/releases/download/v11.5/snapraid-11.5.tar.gz
 tar xzvf snapraid*.tar.gz
 cd snapraid-11.5/
@@ -14,7 +15,7 @@ make
 make check
 make install
 cd ..
-cp ~/snapraid-11.5/snapraid.conf.example /etc/snapraid.conf
+sudo cp ~/snapraid-11.5/snapraid.conf.example /etc/snapraid.conf.example
 cd ..
 rm -rf snapraid*
 # Get drive IDs
@@ -22,15 +23,15 @@ rm -rf snapraid*
 # get SnapRAID config
 sudo wget -P /etc https://raw.githubusercontent.com/zilexa/Mediaserver/master/snapraid.conf
 # SnapRAID create path for local content file
-mkdir -p /var/snapraid/
+sudo mkdir -p /var/snapraid/
 
 # install nocache - required to move files from pool to archive with rsync
 sudo apt -y install nocache
 
 # Create the required folders to mount the disks and MergerFS pools
-mkdir -p /mnt/pool
-mkdir -p /mnt/pool-archive
-mkdir -p /mnt/disks/{data_Cache,data_TopLeft,data_TopRight,parity_BottomLeft,parity_BottomRight}
+sudo mkdir -p /mnt/pool
+sudo mkdir -p /mnt/pool-archive
+sudo mkdir -p /mnt/disks/{data_Cache,data_TopLeft,data_TopRight,parity_BottomLeft,parity_BottomRight}
 
 
 # Configure etc/fstab FIRST (open new terminal for the user, pause the current)
