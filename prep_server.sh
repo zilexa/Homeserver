d126e3b4b0bf2dcfaa512e56ade8786d394f5b0a
ZileXa <zilexa@gmx.com>
2020-12-29 21:26:52 +0100

    Update prep_server.sh

--- prep_server.sh
+++ prep_server.sh
@@ -4,12 +4,18 @@
 # Run this script with sudo -E to make sure $HOME points to /home/username instead of /root
 #
 # Install SSH
+# -----------
 sudo apt -y install ssh
 sudo systemctl enable --now ssh
 # firewall of Ubuntu is disabled by default, I keep it like that but do add the rule in case fw is activated in the future.
 sudo ufw allow ssh 
 
+# Install NFS Server - 15%-30% faster than SAMBA/SMB shares
+sudo apt -y install nfs-server
+read -p "for further instructions to enable NFSv4.2 go to https://github.com/zilexa/Homeserver/blob/master/NetworkFileSystem/NFSv4.2-How-To.md"
+
 # Install Powertop
+# ----------------
 sudo apt -y install powertop
 # Create a service file to run powertop --auto-tune at boot
 cat << EOF | sudo tee /etc/systemd/system/powertop.service
@@ -26,11 +32,12 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable powertop.service
-
 # Tune system now
 sudo powertop --auto-tune
 
+
 # Install Docker, Docker-Compose and bash completion for Compose
+# --------------------------------------------------------------
 wget -qO - https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt -y update
@@ -53,6 +60,7 @@ sudo wget -P $HOME/docker https://raw.githubusercontent.com/zilexa/Homeserver/ma
 sudo wget -P $HOME/docker https://raw.githubusercontent.com/zilexa/Homeserver/master/docker-compose.yml
 
 # AdGuardHome ~ Requirements to run a DNS server on Ubuntu
+# --------------------------------------------------------
 sudo systemctl disable systemd-resolved.service
 sudo systemctl stop systemd-resolved.service
 echo "dns=default" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
@@ -62,7 +70,8 @@ sudo nano /etc/NetworkManager/NetworkManager.conf
 sudo rm /etc/resolv.conf
 sudo systemctl restart NetworkManager.service
 
-# Create Traefik files and set permissions
+# Traefik ~ Create Traefik files and set permissions
+# --------------------------------------------------
 sudo mkdir -p $HOME/docker/traefik
 sudo touch $HOME/docker/traefik/traefik.log
 sudo chmod 600 $HOME/docker/traefik/traefik.log
@@ -71,12 +80,13 @@ sudo chmod 600 $HOME/docker/traefik/acme.json
 # Download Traefik settings
 sudo wget -P $HOME/docker/traefik https://raw.githubusercontent.com/zilexa/Mediaserver/master/traefik/traefik.toml
 
-# Create Firefox-Syncserver file & generate secret
+# Firefox ~ Create Firefox-Syncserver file & generate secret
+# ------------------------------------------------
 sudo mkdir -p $HOME/docker/firefox-syncserver/secret
 sudo touch $HOME/docker/firefox-syncserver/secret/secret.txt
 sudo head -c 20 /dev/urandom | sha1sum | awk '{print $1}' >> $HOME/docker/firefox-syncserver/secret/secret.txt
 
-# Requirement for FileRun, ElasticSearch (= additional container required by FileRun to search by text within files)
+# FileRun ~ requirements also for ElasticSearch
 # Create folder and set permissions
 sudo mkdir -p $HOME/docker/filerun/esearch
 sudo chown -R $USER:$USER $HOME/docker/filerun/esearch
@@ -94,3 +104,26 @@ sudo sh -c "echo 'vm.max_map_count=262144' >> /etc/sysctl.conf"
 # mv dockprom-master/prometheus $HOME/docker
 # rm -r dockprom-master
 # rm -r master.zip
+
+#-----------------------------------
+# Now install services that run bare
+#----------------------------------- 
+# COMMENTED OUT!! Because Syncthing is already installed during Ubuntu Budgie Config: https://github.com/zilexa/UbuntuBudgie-config
+## Add Syncthing repository
+# curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+# echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+# printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing
+# sudo apt -y update
+## Install Syncthing
+# mkdir $HOME/.local/share/syncthing
+#sudo apt -y install syncthing
+#sudo wget -O /etc/systemd/system/syncthing@.service https://raw.githubusercontent.com/zilexa/UbuntuBudgie-config/master/syncthing/syncthing%40.service
+## Start Syncthing at boot
+#sudo systemctl enable syncthing@.service
+#sudo ln -s /etc/systemd/system/syncthing@.service /etc/systemd/system/multi-user.target.wants/syncthing@$LOGNAME.service
+
+# AdGuardHome
+curl -sSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh
+
+# PiVPN 
+ curl -L https://install.pivpn.io | bash  
