3958865d384a4aa0e55c3c32986cd4308e16d7eb
ZileXa <zilexa@gmx.com>
2021-01-25 16:54:34 +0100

    Update prep-folderstructure.sh

--- prep-folderstructure.sh
+++ prep-folderstructure.sh
@@ -24,48 +24,54 @@ echo "Enter the name of the second workstation user (example: Fish), followed by
 read NAME2
 echo USER2='"'$NAME2'"' >> /etc/environment
 
-# This folder will contain precious, long archived files that should be backupped offline, online and offsite. These are also usually to big to keep sync to multiple devices like laptops, phones.
-mkdir -p /mnt/pool/Collections
-mkdir -p /mnt/pool/Collections/Music
-mkdir -p /mnt/pool/Collections/Photos
-
 # This folder will contain files of all users. Each folder is the private cloud of each user. These folders can be synced and/or easily accessed via apps. 
 # Collections can be made available as subdirs within each User folder, via symlink (we will get there). 
+# Differentiate between LOCAL users, which not necessarily have to be in your local network, but simply means these user folders will be treated like your own user folder: included in every backup mechanism.
+# External users can be users for which your server IS their backup. 
 mkdir -p /mnt/pool/Users/Local
 mkdir -p /mnt/pool/Users/External
 mkdir -p /mnt/pool/Users/Local/$NAME1
 mkdir -p /mnt/pool/Users/Local/$NAME2
 
+# If you are part of a family that has shared files, like a Music collection, Photo albums or even files on the Desktop on a shared laptop, 
+# they can be hard to incorporate in your folder structure. I tried many ways, creating a seperate "Collections" folder, but eventually settled on creating a
+# fictious user account "Asterix", the same name we use on our laptop as useraccount. 
+# The benefit is that you maintain 1 single folder structure for all users, making backup organisation, filesystem organisation
+# and local and online sharing just as easy as it is for all other users (1 method for all). 
+mkdir -p /mnt/pool/Users/Local/Asterix
+mkdir -p /mnt/pool/Users/Local/Asterix/Music
+mkdir -p /mnt/pool/Users/Local/Asterix/Photos
+mkdir -p /mnt/pool/Users/LocalAsterix/Desktop
+mkdir -p /mnt/pool/Users/LocalAsterix/Downloads
+
 # If you plan to download series/movies, create a seperate folder structure for it as the material is not unique and not bound to a user and does not require extensive backups. 
 # Note the subdirs have been specifically chosen this way to work perfectly with common download tools. Recommend to stick to it exactly.
-mkdir -p /mnt/pool/Media
-mkdir -p /mnt/pool/Media/incoming
-mkdir -p /mnt/pool/Media/incoming/complete
-mkdir -p /mnt/pool/Media/incoming/incomplete
-mkdir -p /mnt/pool/Media/incoming/blackhole
-mkdir -p /mnt/pool/Media/TVshows
-mkdir -p /mnt/pool/Media/Movies
-
-# If 2 users share the same device (laptop) but use a single login account on that laptop, create a 3rd user with those specific folders (like Desktop)
-# In this example, our login account name is Asterix so we use that as 3rd virtual user folder. 
-# This is also the laptop account name I use for all our shared devices (desktop, laptop). 
-# This way, you can have those folders like Desktop and Downloads on multiple devices and keep them in sync easily. 
-mkdir -p /mnt/pool/Users/Local/Asterix
-mkdir -p /mnt/pool/Users/Local/Asterix/Desktop
-mkdir -p /mnt/pool/Users/Local/Asterix/Downloads
+# NOTE: recently I chose to use the Asterix account for this, instead of a seperate /Media folder next to the /Users folder.
+# This reduces the complexity of the folder structure to an absolute minium. 
+# Note this does not mean tvseries/movies will be backupped. There is a common list of stuff that should never be backupped (OS system files that are scattered throughout user folders). 
+# The TV dir is simply added to that list. 
+mkdir -p /mnt/pool/Users/Local/Asterix/TV
+mkdir -p /mnt/pool/Users/Local/Asterix/TV/TVshows
+mkdir -p /mnt/pool/Users/Local/Asterix/TV/Movies
+mkdir -p /mnt/pool/Users/Local/Asterix/TV/incoming
+mkdir -p /mnt/pool/Users/Local/Asterix/TV/incoming/complete
+mkdir -p /mnt/pool/Users/Local/Asterix/TV/incoming/incomplete
+mkdir -p /mnt/pool/Users/Local/Asterix/TV/incoming/blackhole
 
 # (optional) if you plan to use this server as workstation/desktop, you will use the Home/Username/ personal folders. 
-# Use the MergerFS Pool to store the Home folders of this local PC, by creating symbolic links from Pool to $HOME. First delete the folders.
-mv $HOME/Downloads/* /mnt/pool/Local/Users/Asterix/Downloads/
-mv $HOME/Pictures/* /mnt/pool/Collections/Photos/
-mv $HOME/Music/* /mnt/pool/Collections/Music/
-mv $HOME/Videos/* /mnt/pool/Media/
+# This means you need to map the fictious Asterix users' personal folders to the OS $HOME directory.. That's easy and pretty common on Linux: replace the folders for symbolic links.
+# In case you already have data in those folders, move it to the pool: 
 mv $HOME/Desktop/* /mnt/pool/Local/Users/Asterix/Desktop/
+mv $HOME/Downloads/* /mnt/pool/Local/Users/Asterix/Downloads/
+mv $HOME/Pictures/* /mnt/pool/Local/Users/Asterix/Photos/
+mv $HOME/Music/* /mnt/pool/Local/Users/Asterix//Music/
+mv $HOME/Videos/* /mnt/pool/Local/Users/Asterix/TV/
 
-# Note I did not delete Documents, because we can use that folder as a 'container' for the 2 (or multiple) users sharing this workstation/desktop. 
-# If you prefer you can delete Documents and replace it with a symbolic link to your user folder. 
+# Note I did not delete Documents, because we can use that folder as a 'container' for the 2 (or more) users sharing this workstation/desktop. 
+# If you prefer you can delete Documents and replace it with a symbolic link to your user folder /Asterix/Documents.
 
-# Now create several links, remember, the login account is actually Asterix so now we will link Asterix sub folders when available. 
+# Now create several links, remember, the login account is actually Asterix in this example, so now we will link Asterix sub folders when available. 
+# $HOME = a system variable and short for /home/asterix or whatever username you choose during OS installation. 
 ln -s /mnt/pool/Collections/Music $HOME/
 ln -s /mnt/pool/Collections/Photos $HOME/
 ln -s /mnt/pool/Media $HOME/
@@ -78,7 +84,10 @@ sudo sed -i -e 's+$HOME/Desktop+$HOME/Documents/Desktop+g' $HOME/.config/user-di
 ln -s /mnt/pool/Users/Local/Asterix/Desktop $HOME/
 sudo sed -i -e 's+$HOME/Documents/Desktop+$HOME/Desktop+g' $HOME/.config/user-dirs.dirs
 
-# Now on a client PC, like a laptop that is shared by NAME1 and NAME2, add symlinks after setting up NFS shares
+# Now on a client PC, like a laptop that is shared by NAME1 and NAME2, add symlinks after setting up NFS shares (see NFS v4.2 guide)
+# You only have to do this for folders with lots of files, as you don't want to fill up the limited space of your laptop.
+# Other stuff like documents can be 2-way synced via Syncthing. 
 #ln -s /mnt/Obelix/Collections/Photos/ $HOME/Photos/Obelix
 #ln -s /mnt/Obelix/Collections/Music/ $HOME/Music/Obelix
 #ln -s /mnt/Obelix/Media/ $HOME/Media/Obelix
+
