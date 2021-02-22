ab57ed5189847c03cc6bfa91272c2866c99737e8
ZileXa <zilexa@gmx.com>
2021-02-22 10:06:21 +0100

    Update backup.sh

--- backup system/backup.sh
+++ backup system/backup.sh
@@ -13,11 +13,10 @@ mount -U 17aa9385-fa24-4705-b6c0-f87d5d3f2fd5 /mnt/disks/backup1 -o subvolid=5,d
 # BACKUP OF LOCAL USERS
 # ---------------------
 # first use rsync to backup from MergerFS pool to backup drive
-nocache rsync -azXA --info=progress2 --delete --inplace --numeric-ids --exclude '/mnt/pool/Users/Local/Asterix/TV' /mnt/pool/Users/Local/ /mnt/disks/backup1/users.backup/
+nocache rsync -azXA --progress --delete --inplace --numeric-ids --exclude '/mnt/pool/Users/Local/Asterix/TV' /mnt/pool/Users/Local/ /mnt/disks/backup1/users.backup/
 
 # Create snapshots of synced users data
-btrbk -c /home/asterix/docker/HOST/users-backup.conf -v run
-
+/usr/sbin/btrbk -c /home/asterix/docker/HOST/users-backup.conf -v run
 
 # BACKUP OF SYSTEM
 # ---------------------
@@ -25,7 +24,7 @@ btrbk -c /home/asterix/docker/HOST/users-backup.conf -v run
 mount /dev/nvme0n1p2 /mnt/btrfs-root -o subvolid=5,defaults,noatime,compress=lzo 
 
 # Create snaphots & backups of system drive and data Pool local users
-btrbk -c /home/asterix/docker/HOST/system-backup.conf -v run
+/usr/sbin/btrbk -c /home/asterix/docker/HOST/system-backup.conf -v run
 
 # Unmount btrfs root and backup drive
 umount /mnt/disks/backup1
