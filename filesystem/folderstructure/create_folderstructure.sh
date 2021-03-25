5ca0c17ba26ccca2e4d3301b77547c9b7eb101a4
ZileXa <zilexa@gmx.com>
2021-03-25 23:33:27 +0100

    Update create_folderstructure.sh

--- filesystem/folderstructure/create_folderstructure.sh
+++ filesystem/folderstructure/create_folderstructure.sh
@@ -2,25 +2,42 @@
 # Permissions can be fixed as follows (this makes sure the personal folder cannot be deleted by the main user, but gives full access to the main user to the contents of the folder. 
 # sudo chown asterix:asterix $NAME
 # sudo chmod u+rwx -R $NAME
-
-echo "Enter the name of the first workstation user (example: Your first name), followed by [ENTER]:"
+echo "==========================================================================================================="
+echo "If you are part of a family that has shared files, like Photo albums, Documents, files on Desktop, Downloads..." 
+echo "they can be hard to incorporate in your folder structure." 
+echo "Solve this by creating an extra user folder, besides the folders of you and your partner. Just call it Shared or come up with a funky name."
+echo "This script will create the user folders for you, your partner and that made up user." 
+echo "Next, it will create the common personal folders inside the made up user folder." 
+echo "Then it will replace the common personal documents in your Home folder with links to this virtual user in your pool." 
+echo "This virtual user folder can be seen as the shared folder, you can map it via Docker Compose (FileRun, see example in compose) to both of your user folders. 
+echo "You can even share tis virtual user folder over the LAN network via SMB or NFS." 
+echo " "
+echo "The benefit is that you maintain 1 single folder structure for all users, making backup organisation, filesystem organisation" 
+echo "and local and online sharing just as easy as it is for all other users (1 method for all)." 
+echo "==========================================================================================================="
+read -p "Read between the lines then HIT CTRL+C to stop the script here or hit ENTER to start, you will be asked to enter names.. "
+echo "==========================================================================================================="
+echo "Enter your name followed by ENTER. First letter capital, this will be /mnt/pool/Users/Yourname and a symlink will be added to the $HOME folder:"
 read NAME1
 echo USER1='"'$NAME1'"' >> /etc/environment
-echo "Enter the name of the second workstation user (example: Your partners name), followed by [ENTER]:"
+echo "-----------------------------------------------------------------------------------------------------------"
+echo "Now enter the name of your partner or other person that can use this workstation, same actions will be performed:"
 read NAME2
 echo USER2='"'$NAME2'"' >> /etc/environment
+echo "-----------------------------------------------------------------------------------------------------------"
+echo "Now come up with a name for the shared user folder, for example Batman." 
+echo "This workstation common personal folders (Desktop, Documents etc) will be moved to the pool and symlinked back to the $HOME folder:"
+read NAME2
+echo SHAREDUSER='"'$SHAREDUSER'"' >> /etc/environment
 
 # This folder will contain files of all users. Each folder is the private cloud of each user. These folders can be synced and/or easily accessed via apps. 
 # Collections can be made available as subdirs within each User folder, via symlink (we will get there). 
 mkdir -p /mnt/pool/Users/$NAME1
 mkdir -p /mnt/pool/Users/$NAME2
+mkdir -p /mnt/pool/Users/$SHAREDUSER
 
-# If you are part of a family that has shared files, like a Music collection, Photo albums or even files on the Desktop on a shared laptop, 
-# they can be hard to incorporate in your folder structure. I tried many ways, creating a seperate "Collections" folder, but eventually settled on creating a
-# fictious user account "Asterix", the same name we use on our laptop as useraccount. 
-# The benefit is that you maintain 1 single folder structure for all users, making backup organisation, filesystem organisation
-# and local and online sharing just as easy as it is for all other users (1 method for all). 
-mkdir -p /mnt/pool/Users/Asterix/{Documents,Photos,Desktop,Downloads}
+
+mkdir -p /mnt/pool/Users/$SHAREDUSER/{Documents,Photos,Desktop,Downloads}
 
 
 # If you plan to download series/movies, create a seperate folder structure for it as the material is not unique and not bound to a user and does not require extensive backups. 
@@ -37,10 +54,10 @@ btrfs subvolume create /mnt/pool/TV/incoming/incomplete
 # (optional) if you plan to use this server as workstation/desktop, you will use the Home/Username/ personal folders. 
 # This means you need to map the fictious Asterix users' personal folders to the OS $HOME directory.. That's easy and pretty common on Linux: replace the folders for symbolic links.
 # In case you already have data in those folders, move it to the pool: 
-mv $HOME/Documents/* /mnt/pool/Users/Asterix/Documents/
-mv $HOME/Desktop/* /mnt/pool/Users/Asterix/Desktop/
-mv $HOME/Downloads/* /mnt/pool/Users/Asterix/Downloads/
-mv $HOME/Pictures/* /mnt/pool/Users/Asterix/Photos/
+mv $HOME/Documents/* /mnt/pool/Users/$SHAREDUSER/Documents/
+mv $HOME/Desktop/* /mnt/pool/Users/$SHAREDUSER/Desktop/
+mv $HOME/Downloads/* /mnt/pool/Users/$SHAREDUSER/Downloads/
+mv $HOME/Pictures/* /mnt/pool/Users/$SHAREDUSER/Photos/
 mv $HOME/Music/* /mnt/pool/Music/
 mv $HOME/Videos/* /mnt/pool/TV/
 
@@ -53,10 +70,10 @@ ln -s /mnt/pool/Music $HOME/
 ln -s /mnt/pool/TV $HOME/
 ln -s /mnt/pool/Users/$NAME1 $HOME/
 ln -s /mnt/pool/Users/$NAME2 $HOME/
-ln -s /mnt/pool/Users/Asterix/Documents $HOME/
-ln -s /mnt/pool/Users/Asterix/Desktop $HOME/
-ln -s /mnt/pool/Users/Asterix/Downloads $HOME/
-ln -s /mnt/pool/Users/Asterix/Photos/ $HOME/
+ln -s /mnt/pool/Users/$SHAREDUSER/Documents $HOME/
+ln -s /mnt/pool/Users/$SHAREDUSER/Desktop $HOME/
+ln -s /mnt/pool/Users/$SHAREDUSER/Downloads $HOME/
+ln -s /mnt/pool/Users/$SHAREDUSER/Photos/ $HOME/
 
 # Be warned, the file $HOME/.config/user-dirs.dirs contains the path of your account folders,
 # if you rename folders (like I rename Pictures to Photos) or move folders (I prefer Templates (you can't get rid of it) to be within Documents) you need to change it here as well.
@@ -75,9 +92,9 @@ sudo sed -i -e 's+$HOME/Videos+$HOME/TV+g' $HOME/.config/user-dirs.dirs
 # Note, deleting or moving Desktop is not possible, the folder will be re-created immediately. 
 # Temporarily change the location via a user config file, create the symlink, change it back. 
 sudo sed -i -e 's+$HOME/Desktop+$HOME/Documents/Desktop+g' $HOME/.config/user-dirs.dirs
-ln -s /mnt/pool/Users/Local/Asterix/Desktop $HOME/
+ln -s /mnt/pool/Users/Local/$SHAREDUSER/Desktop $HOME/
 sudo sed -i -e 's+$HOME/Documents/Desktop+$HOME/Desktop+g' $HOME/.config/user-dirs.dirs
 
 # after setting up NFS shares (see NFS v4.2 guide), you can mount the folders that are too large for the laptop/client devices:
 # Other stuff like documents can be 2-way synced via Syncthing. 
-#ln -s /mnt/servername/Asterix/Photos $HOME/
+#ln -s /mnt/servername/$SHAREDUSER/Photos $HOME/
