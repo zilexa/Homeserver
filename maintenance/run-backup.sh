f70f78beff31923876896efe7b42c62ec82ba8bc
ZileXa <zilexa@gmx.com>
2021-03-26 22:08:25 +0100

    Update and rename backup.sh to run-backup.sh

---
+++ maintenance/run-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Seperate config because btrbk requires sudo. 
+
+# Make sure maintenance is finished
+while [[ -f /tmp/maintenance-is-running ]] ; do
+   sleep 10 ;
+done
+
+# Mount systemdrive btrfs-root and the backup disk
+mount /dev/nvme0n1p2 /mnt/system-root -o subvolid=5,defaults,noatime,compress=lzo 
+mount -U !!!UUID OF BACKUPDRIVE HERE!!! /mnt/disks/backup1 -o subvolid=backup,defaults,noatime,compress=zstd:8
+
+# Run backups
+/usr/sbin/btrbk -c /home/$SUDO_USER/docker/HOST/backup.conf -v run
+
+# Unmount btrfs root and backup drive
+umount /mnt/disks/backup1
+umount /mnt/btrfs-root
