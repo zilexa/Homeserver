1e3a5f2424f3a82dbb341fea3abad6ea65a61178
ZileXa <zilexa@gmx.com>
2021-03-26 22:11:41 +0100

    Update and rename run-cleanup-snapraid.sh to run-cleanup.sh

--- maintenance/run-cleanup.sh
+++ maintenance/run-cleanup.sh
@@ -14,10 +14,6 @@ $HOME/docker/HOST/jellyfin-cleaner/media_cleaner.py >> $HOME/docker/HOST/logs/me
 # files >30d moved to data drives on pool-archive
 /usr/bin/bash $HOME/docker/HOST/cache_archiver.sh /mnt/disks/cache/Users /mnt/pool-archive/Users 30
 
-# PROTECT DATA DRIVES - SnapRAID
-# ------------------------------
-# Run SnapRAID
-python3 snapraid-btrfs-runner.py
 
 # Delete temp file, follow up tasks can continue
 rm /tmp/maintenance-is-running
