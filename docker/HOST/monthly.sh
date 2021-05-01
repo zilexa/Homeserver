ae59a9163e1bdeb258a82c44af02dc6ebcec956d
ZileXa <zilexa@gmx.com>
2021-05-01 12:42:36 +0200

    Update monthly.sh

--- docker/HOST/monthly.sh
+++ docker/HOST/monthly.sh
@@ -3,12 +3,12 @@
 #while [[ -f /tmp/backup-is-running ]] ; do
 #   sleep 10 ;
 #done
+
 # Get this script folder path
 SCRIPTDIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
-#
+
 # Create a temp file to indicate maintenance is running
 touch ${SCRIPTDIR}/running-tasks
-
 
 # Cleanup unused docker images and volumes
 # ----------------------------------------
@@ -30,16 +30,16 @@ btrfs scrub start -Bd -c 2 -n 4 /dev/sdb
 
 # Run btrfs balance monthly, first 10% data, then try 20%
 # -------------------------
-run_task btrfs balance start -dusage=10 /dev/nvme0n1p2
-run_task btrfs balance start -v -dusage=20 /dev/nvme0n1p2
-run_task btrfs balance start -dusage=10 /dev/nvme1n1
-run_task btrfs balance start -v -dusage=20 /dev/nvme1n1
-run_task btrfs balance start -dusage=10 /dev/sdc
-run_task btrfs balance start -v -dusage=20 /dev/sdc
-run_task btrfs balance start -dusage=10 /dev/sdd
-run_task btrfs balance start -v -dusage=20 /dev/sdd
-run_task btrfs balance start -dusage=10 /dev/sdb
-run_task btrfs balance start -v -dusage=20 /dev/sdb
+run_task btrfs balance start -dusage=10 -musage=5 /dev/nvme0n1p2
+run_task btrfs balance start -v -dusage=20 -musage=10 /dev/nvme0n1p2
+run_task btrfs balance start -dusage=10 -musage=5 /dev/nvme1n1
+run_task btrfs balance start -v -dusage=20 -musage=10 /dev/nvme1n1
+run_task btrfs balance start -dusage=10 -musage=5 /dev/sdc
+run_task btrfs balance start -v -dusage=20 -musage=10 /dev/sdc
+run_task btrfs balance start -dusage=10 -musage=5 /dev/sdd
+run_task btrfs balance start -v -dusage=20 -musage=10 /dev/sdd
+run_task btrfs balance start -dusage=10 -musage=5 /dev/sdb
+run_task btrfs balance start -v -dusage=20 -musage=10 /dev/sdb
 
 # Delete temp file, follow up tasks can continue
 rm ${SCRIPTDIR}/running-tasks
