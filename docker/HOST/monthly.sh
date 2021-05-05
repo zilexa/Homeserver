6a6335c7c05c4d7cf7c7a839e80760469d74e5c5
ZileXa <zilexa@gmx.com>
2021-05-05 15:22:09 +0200

    Update monthly.sh

--- docker/HOST/monthly.sh
+++ docker/HOST/monthly.sh
@@ -8,33 +8,34 @@ done
 SCRIPTDIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 
-# Create email body, add current date
-# -----------------------------------
-install -b -m 750 /dev/null ${SCRIPTDIR}logs/monthly.txt
-echo "$now" >> ${SCRIPTDIR}/logs/monthly.txt
+# Create monthly email body, add title and current date
+# -----------------------------------------------------
+install -b -m 750 /dev/null ${SCRIPTDIR}/logs/monthly.txt
+echo -e "\nMONTHLY HOUSEKEEPING TASKS\n" >> ${SCRIPTDIR}/logs/monthly.txt
+date >> ${SCRIPTDIR}/logs/monthly.txt
+printf "\n" >> ${SCRIPTDIR}/logs/monthly.txt
 
 
 # CLEANUP - OS, local apps, user profile 
 # --------------------------------------
-sudo bleachbit --preset --clean
-# Keep only the summary of Bleachbit
-tail -n 4 ${SCRIPTDIR}/logs/monthly.txt > ${SCRIPTDIR}/logs/monthly.tmp
-mv -f ${SCRIPTDIR}/logs/monthly.tmp ${SCRIPTDIR}/logs/monthly.txt
-# Add header to this task
-sed -i '1iCLEANUP of OS, local apps and user profile..\n' ${SCRIPTDIR}/logs/monthly.txt
+echo -e "\nBLEACHBIT - Cleanup of OS, local apps and user profile..\n" >> ${SCRIPTDIR}/logs/monthly.txt
+sudo bleachbit --preset --clean |& tee -a ${SCRIPTDIR}/logs/bleachbit.tmp
+# Add the summary of Bleachbit output to our monthly mail
+tail -n 4 ${SCRIPTDIR}/logs/bleachbit.tmp >> ${SCRIPTDIR}/logs/monthly.txt
+sudo rm ${SCRIPTDIR}/logs/bleachbit.tmp
 
 # CLEANUP - unused docker images and volumes 
 # ----------------------------------------
-echo -e "\nCLEANUP unused docker images..\n" >> ${SCRIPTDIR}/logs/monthly.txt
+echo -e "\nCLEANUP of unused docker images..\n" >> ${SCRIPTDIR}/logs/monthly.txt
 docker image prune -a -f |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-echo -e "\nCLEANUP unused docker volumes..\n" >> ${SCRIPTDIR}/logs/monthly.txt
+echo -e "\nCLEANUP of unused docker volumes..\n" >> ${SCRIPTDIR}/logs/monthly.txt
 docker volume prune -f |& tee -a ${SCRIPTDIR}/logs/monthly.txt
 echo -e "\nFor a full cleanup, remember to regularly run this command after verifying all your containers are running: docker system prune --all --volumes -f\n" >> ${SCRIPTDIR}/logs/monthly.txt
 
 
 # Check docker registry for image updates and send notifications
 # --------------------------------------------------------------
-diun
+sudo diun
 
 
 # Run btrfs scrub monthly
@@ -44,25 +45,23 @@ sudo btrfs scrub start -Bd -c 2 -n 4 /dev/nvme0n1p2 |& tee -a ${SCRIPTDIR}/logs/
 sudo btrfs scrub start -Bd -c 2 -n 4 /dev/nvme1n1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
 sudo btrfs scrub start -Bd -c 2 -n 4 /dev/sdc |& tee -a ${SCRIPTDIR}/logs/monthly.txt
 sudo btrfs scrub start -Bd -c 2 -n 4 /dev/sdd |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs scrub start -Bd -c 2 -n 4 /dev/sdb |& tee -a ${SCRIPTDIR}/logs/monthly.txt
 
 # Run btrfs balance monthly, first 10% data, then try 20%
 # -------------------------
 echo -e "\nBalance btrfs filesystems in 2 runs each.. \n" >> ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -dusage=10 -musage=5 /dev/nvme0n1p2 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -v -dusage=20 -musage=10 /dev/nvme0n1p2 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -dusage=10 -musage=5 /dev/nvme1n1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -v -dusage=20 -musage=10 /dev/nvme1n1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -dusage=10 -musage=5 /dev/sdc |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -v -dusage=20 -musage=10 /dev/sdc |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -dusage=10 -musage=5 /dev/sdd |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -v -dusage=20 -musage=10 /dev/sdd |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -dusage=10 -musage=5 /dev/sdb |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo btrfs balance start -v -dusage=20 -musage=10 /dev/sdb |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo btrfs balance start -dusage=10 -musage=5 / |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo btrfs balance start -v -dusage=20 -musage=10 / |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo btrfs balance start -dusage=10 -musage=5 /mnt/disks/data0 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo btrfs balance start -v -dusage=20 -musage=10 /mnt/disks/data0 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo btrfs balance start -dusage=10 -musage=5 /mnt/disks/data1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo btrfs balance start -v -dusage=20 -musage=10 /mnt/disks/data1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo btrfs balance start -dusage=10 -musage=5 /mnt/disks/data2 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo btrfs balance start -v -dusage=20 -musage=10 /mnt/disks/data2 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+
 
 # Send email
 # ---------------------------------
-s-nail -s "Obelix monthly tasks" < $HOME/docker/HOST/logs/monthly.txt default
+s-nail -s "Obelix Server - monthly housekeeping" < ${SCRIPTDIR}/logs/monthly.txt default
 
 
 # Append email to monthly logfile and delete email
