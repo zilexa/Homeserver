3181f98a6bd30889d8c3cfded3d2061407257945
ZileXa <zilexa@gmx.com>
2021-05-07 13:48:16 +0200

    Update nightly.sh

--- docker/HOST/nightly.sh
+++ docker/HOST/nightly.sh
@@ -14,7 +14,7 @@ touch ${SCRIPTDIR}/running-tasks
 # CLEANUP WATCHED TVSHOWS & MOVIES
 # --------------------------------
 # delete if watched x days ago
-${SCRIPTDIR}/media-cleaner/media_cleaner.py >> ${SCRIPTDIR}/logs/media_cleaner.log
+python3 ${SCRIPTDIR}/media-cleaner/media_cleaner.py |& tee -a ${SCRIPTDIR}/logs/media_cleaner.log
 
 
 # CLEANUP CACHE
@@ -26,15 +26,15 @@ ${SCRIPTDIR}/media-cleaner/media_cleaner.py >> ${SCRIPTDIR}/logs/media_cleaner.l
 # FileRun 
 # -------
 # Empty trash >30 days old files
-docker exec -w /var/www/html/cron -it filerun php empty_trash.php -days 30
+docker exec -w /var/www/html/cron filerun php empty_trash.php -days 30
 # Clear db of files/folders that no longer exist
-docker exec -w /var/www/html/cron -it filerun php paths_cleanup.php --deep
+docker exec -w /var/www/html/cron filerun php paths_cleanup.php --deep
 # Index filenames for files created outside FileRun
-docker exec -w /var/www/html/cron -it filerun php index_filenames.php /user-files true
+docker exec -w /var/www/html/cron filerun php index_filenames.php /user-files true
 # Read metadata of files created outside FileRun, the UI adjusts to photos (GPS), videos etc and has specific options per filetype
-docker exec -w /var/www/html/cron -it filerun php metadata_index.php
+docker exec -w /var/www/html/cron filerun php metadata_index.php
 # Create thumbnails for photos - allows instant scrolling through photos
-docker exec -w /var/www/html/cron -it filerun php make_thumbs.php
+docker exec -w /var/www/html/cron filerun php make_thumbs.php
 # Index content of files, extracting text, to allow searching within files - not recommended
 # usr/bin/docker exec -w /var/www/html/cron -it filerun php process_search_index_queue.php
 
