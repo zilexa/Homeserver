df83fe57c0ce21c60bf4351d64556049ee00f2b9
ZileXa <zilexa@gmx.com>
2021-06-25 14:25:38 +0200

    Create updateport-qbittorrent.sh

---
+++ docker/vpn-proxy/pia-shared/updateport-qbittorrent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+port="$1"
+QBT_USER=
+QBT_PASS=
+QBT_PORT=9090
+
+echo "Setting qBittorrent port settings ($port)..."
+# Very basic retry logic so we don't fail if qBittorrent isn't running yet
+ while ! curl --silent --retry 10 --retry-delay 15 --max-time 10 \
+  --data "username=${QBT_USER}&password=${QBT_PASS}" \
+  --cookie-jar /tmp/qb-cookies.txt \
+  http://localhost:${QBT_PORT}/api/v2/auth/login
+  do
+    sleep 10
+  done
+  
+curl --silent --retry 10 --retry-delay 15 --max-time 10 \
+  --data 'json={"listen_port": "'"$port"'"}' \
+  --cookie /tmp/qb-cookies.txt \
+  http://localhost:${QBT_PORT}/api/v2/app/setPreferences
+
+echo "Qbittorrent port updated successfully ($port)..."
