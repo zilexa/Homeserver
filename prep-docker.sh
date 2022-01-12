0b22cf3741cf57fab62afc972a9e839f30d49057
ZileXa <zilexa@gmx.com>
2022-01-12 11:13:51 +0100

    Update prep-docker.sh

--- prep-docker.sh
+++ prep-docker.sh
@@ -1,30 +1,5 @@
- >>> DO NOT USE THIS FILE. USE /ARCHIVED/ INSTEAD. THIS FILE IS BEING ADJUSTED FOR MANJARO LINUX. 
+ >>> DO NOT USE THIS FILE ON UBUNTU. USE /ARCHIVED/ INSTEAD. THIS FILE IS BEING ADJUSTED FOR MANJARO LINUX. 
 #!/bin/bash
-
-echo "_________________________________________________________________________"
-echo "                    ISOLATE DOCKER PERSISTENT DATA                       "
-echo "              Create subvolume for Docker persistent data                "
-echo "_________________________________________________________________________"
-# Create a folder to temporarily map the BTRFS root
-sudo mkdir /mnt/system
-# Mount BTRFS root
-SYSTEMDRIVE=$(df / | grep / | cut -d" " -f1)
-sudo mount -o subvolid=5 $SYSTEMDRIVE /mnt/system
-# create a root subvolume for docker
-sudo btrfs subvolume create /mnt/system/@docker
-## unmount root filesystem
-sudo umount /mnt/system
-# Get system fs UUID
-fs_uuid=$(findmnt / -o UUID -n)
-# Add @docker subvolume to fstab to mount at boot
-sudo tee -a /etc/fstab &>/dev/null << EOF
-# Mount the BTRFS root subvolume @userdata
-UUID=${fs_uuid} /mnt/docker  btrfs   defaults,noatime,subvol=@docker,compress-force=zstd:1  0  0
-EOF
-
-# Create folder for server maintenance scripts
-mkdir -p $HOME/docker/HOST/system/etc
-
 
 echo "______________________________________________________"
 echo "        MANAGE POWER CONSUMPTION AUTOMATICALLY        "
@@ -51,88 +26,10 @@ sudo powertop --auto-tune
 ## Start the service
 sudo systemctl start powertop.service
 
-echo "______________________________________________________"
-echo "                     APPLICATIONS                     "
-echo "                     server tools                     "
-echo "______________________________________________________"
-echo "Run-if-today: simplify scheduling of weekly or monthly tasks"
-sudo wget -O /usr/bin/run-if-today https://raw.githubusercontent.com/xr09/cron-last-sunday/master/run-if-today
-sudo chmod +x /usr/bin/run-if-today
-
-echo "btrbk - flexible tool to automate snapshots & backups "
-sudo pamac install --no-confirm btrbk
-## Get config and email script
-mkdir -p $HOME/docker/HOST/btrbk
-wget -O $HOME/docker/HOST/btrbk/btrbk.conf https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/HOST/btrbk/btrbk.conf
-wget -O $HOME/docker/HOST/btrbk/btrbk-mail.sh https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/HOST/btrbk/btrbk-mail.sh
-sudo ln -s $HOME/docker/HOST/btrbk/btrbk.conf /etc/btrbk/btrbk.conf
-# MANUALLY configure the $HOME/docker/HOST/btrbk/btrbk.conf to your needs
-
-echo "nocache - handy when moving lots of files at once in the background, without filling up cache and slowing down the system."
-sudo pamac install --no-confirm nocache
-
-echo "Grync - friendly UI for rsync"
-sudo pacman -S --noconfirm grsync
-
-echo "lm_sensors to be able to read out all sensors" 
-sudo pacman -S --noconfirm lm_sensors
-sudo sensors-detect --auto
-
-
-echo "____________________________________________________"
-echo "                    APPLICATIONS                    "
-echo "                      DOCKER                        " 
-echo "____________________________________________________"
-echo " Install docker, docker-compose and docker-rootless-extras" 
-sudo pacman -S --noconfirm docker docker-compose
-pamac install docker-rootless-extras-bin
-# Required steps before running docker rootless setup tool
-sudo touch /etc/subuid
-sudo touch /etc/subgid
-echo "${USER}:100000:65536" | sudo tee -a /etc/subuid
-echo "${USER}:100000:65536" | sudo tee -a /etc/subgid
-systemctl --user enable --now docker.socket
-export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
-# Run docker rootless setup tool
-dockerd-rootless-setuptool.sh install
-# enable start at boot and start docker
-systemctl --user enable docker
-systemctl --user start docker
-sudo loginctl enable-linger $(whoami)
-
-echo "Configure Docker"
-# Create the docker folder
-sudo mkdir -p $HOME/docker
-sudo setfacl -Rdm g:docker:rwx $HOME/docker
-sudo chmod -R 755 $HOME/docker
-# Get environment variables to be used by Docker (i.e. requires TZ in quotes)
-sudo wget -O $HOME/docker/.env https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/.env
-# Get docker compose file
-sudo wget -O $HOME/docker/docker-compose.yml https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/docker-compose.yml
-
-echo "Pullio (auto-update labeled containers) & Diun (notify of image updates for labeled containers)"
-mkdir -P $HOME/docker/HOST/updater
-cd $HOME/Downloads
-wget -qO- https://github.com/crazy-max/diun/releases/download/v4.15.2/diun_4.15.2_linux_x86_64.tar.gz | tar -zxvf - diun
-sudo cp diun $HOME/docker/HOST/updater/
-sudo ln -s $HOME/docker/HOST/updater/diun /usr/local/bin/diun
-rm diun*.gz
-rm diun
-# Get Diun conf file
-wget -O $HOME/docker/HOST/updater/diun.yml https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/HOST/diun/diun.yml
-sudo chmod 770 $HOME/docker/HOST/updater/diun.yml
-sudo mkdir /etc/diun
-sudo chmod 770 /etc/diun
-sudo ln -s $HOME/docker/HOST/updater/diun.yml /etc/diun/diun.yml
-# Install Pullio to auto update a few services
-sudo wget -O $HOME/docker/HOST/updater/pullio https://raw.githubusercontent.com/hotio/pullio/master/pullio.sh
-sudo chmod +x $HOME/docker/HOST/updater/pullio
-sudo ln -s $HOME/docker/HOST/updater/pullio /usr/local/bin/pullio
-
 
 echo "_____________________________________________________________"
 echo "                     SENDING EMAILS                          "
-echo "  Configure linux email notifications without heavy postfix  " 
+echo "       allow system to send email notifications              " 
 echo "_____________________________________________________________"
 # ----------------------------
 sudo pacman -S --noconfirm msmtp
@@ -144,10 +41,113 @@ sudo ln -s /usr/bin/msmtp /usr/sbin/sendmail
 echo "set mta=/usr/bin/msmtp" | sudo tee -a /etc/mail.rc
 ## Get config file, MANUALLY add your smtp provider credentials
 sudo wget -O /etc/msmtprc https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/HOST/system/etc/msmtprc
-# Create aliases file, MANUALLY edit file and replace with your emailaddress 
+echo "Create aliases file, MANUALLY edit file and replace with your emailaddress" 
 sudo tee -a /etc/aliases << EOF
 default:myemail@address.com
 EOF
+
+
+echo "____________________________________________"
+echo "                APPLICATIONS                "
+echo "                server tools                "
+echo "____________________________________________"
+echo "                   BTRBK                    "
+echo "--------------------------------------------"
+echo "Swiss handknife-like tool to automate snapshots & backups of personal data" 
+sudo pamac install --no-confirm btrbk
+## Get config and email script
+mkdir -p $HOME/docker/HOST/btrbk
+wget -O $HOME/docker/HOST/btrbk/btrbk.conf https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/HOST/btrbk/btrbk.conf
+wget -O $HOME/docker/HOST/btrbk/btrbk-mail.sh https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/HOST/btrbk/btrbk-mail.sh
+sudo ln -s $HOME/docker/HOST/btrbk/btrbk.conf /etc/btrbk/btrbk.conf
+# MANUALLY configure the $HOME/docker/HOST/btrbk/btrbk.conf to your needs
+
+echo "               RUN-IF-TODAY                 "
+echo "--------------------------------------------"
+echo "simplify scheduling of weekly/monthly tasks"
+sudo wget -O /usr/bin/run-if-today https://raw.githubusercontent.com/xr09/cron-last-sunday/master/run-if-today
+sudo chmod +x /usr/bin/run-if-today
+
+echo "                   NOCACHE                  "
+echo "--------------------------------------------"
+echo "handy when moving lots of files at once in the background, without filling up cache and slowing down the system."
+sudo pamac install --no-confirm nocache
+
+echo "                    GRSYNC                  "
+echo "--------------------------------------------"
+echo "Friendly UI for rsync"
+sudo pacman -S --noconfirm grsync
+
+echo "                LM_SENSORS                  "
+echo "--------------------------------------------"
+echo "to be able to read out all sensors" 
+sudo pacman -S --noconfirm lm_sensors
+sudo sensors-detect --auto
+
+
+echo "____________________________________________"
+echo "                APPLICATIONS                "
+echo "                   docker                   "
+echo "____________________________________________"
+echo "       ISOLATE DOCKER PERSISTENT DATA       "
+echo "--------------------------------------------"
+echo "create subvolume for Docker persistent data "
+# Create a folder to temporarily map the BTRFS root
+sudo mkdir /mnt/system
+# Mount BTRFS root
+SYSTEMDRIVE=$(df / | grep / | cut -d" " -f1)
+sudo mount -o subvolid=5 $SYSTEMDRIVE /mnt/system
+# create a root subvolume for docker
+sudo btrfs subvolume create /mnt/system/@docker
+## unmount root filesystem
+sudo umount /mnt/system
+# Get system fs UUID
+fs_uuid=$(findmnt / -o UUID -n)
+# Add @docker subvolume to fstab to mount at boot
+sudo tee -a /etc/fstab &>/dev/null << EOF
+# Mount the BTRFS root subvolume @userdata
+UUID=${fs_uuid} /mnt/docker  btrfs   defaults,noatime,subvol=@docker,compress-force=zstd:1  0  0
+EOF
+
+echo "Create folder for server maintenance scripts"
+mkdir -p $HOME/docker/HOST/system/etc
+
+echo "Configure Docker folder" 
+#sudo setfacl -Rdm g:docker:rwx $HOME/docker
+#sudo chmod -R 755 $HOME/docker
+
+echo "              INSTALL DOCKER                "
+echo "--------------------------------------------"
+sudo pacman -S --noconfirm docker docker-compose
+pamac install docker-rootless-extras-bin
+
+# Required steps before running docker rootless setup tool
+sudo touch /etc/subuid
+sudo touch /etc/subgid
+echo "${USER}:100000:65536" | sudo tee -a /etc/subuid
+echo "${USER}:100000:65536" | sudo tee -a /etc/subgid
+systemctl --user enable --now docker.socket
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+
+# Run docker rootless setup tool
+dockerd-rootless-setuptool.sh install
+
+# enable start at boot and start docker
+systemctl --user enable docker
+systemctl --user start docker
+sudo loginctl enable-linger $(whoami)
+
+
+echo "Get docker compose file and its environment variables" 
+sudo wget -O $HOME/docker/.env https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/.env
+sudo wget -O $HOME/docker/docker-compose.yml https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/docker-compose.yml
+
+
+echo "Install Pullio (auto-update labeled containers)"
+# Should only be used for selected services. For all others, Diun (docker container) is used to notify only instead of auto-update.
+sudo wget -O $HOME/docker/HOST/updater/pullio https://raw.githubusercontent.com/hotio/pullio/master/pullio.sh
+sudo chmod +x $HOME/docker/HOST/updater/pullio
+sudo ln -s $HOME/docker/HOST/updater/pullio /usr/local/bin/pullio
 
 
 echo "______________________________________________________"
