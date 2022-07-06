7e03e6626198896588f981bd20b69331a161cce9
ZileXa <zilexa@gmx.com>
2022-07-06 18:44:44 +0200

    Update nightly.sh

--- docker/HOST/nightly.sh
+++ docker/HOST/nightly.sh
@@ -12,7 +12,7 @@ touch ${SCRIPTDIR}/running-tasks
 # CLEANUP WATCHED TVSHOWS & MOVIES
 # --------------------------------
 # delete if watched x days ago
-python3 ${SCRIPTDIR}/media-cleaner/media_cleaner.py |& tee -a ${SCRIPTDIR}/logs/media_cleaner.log
+python3 ${SCRIPTDIR}/mediacleaner/media_cleaner.py |& tee -a ${SCRIPTDIR}/logs/media_cleaner.log
 
 
 # CLEANUP CACHE
@@ -31,13 +31,15 @@ docker exec -w /var/www/html/cron filerun php paths_cleanup.php --deep
 docker exec -w /var/www/html/cron filerun php index_filenames.php /user-files true
 # Read metadata of files created outside FileRun, the UI adjusts to photos (GPS), videos etc and has specific options per filetype
 docker exec -w /var/www/html/cron filerun php metadata_index.php 
-# Create thumbnails for files - allows instant scrolling through photos
+# Create thumbnails for files
 docker exec -w /var/www/html/cron filerun php make_thumbs.php
-# Create previews for files - allows instant previews for photos
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username FiLeRuNuSeR1 --size large
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username FiLeRuNuSeR2 --size large
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username FiLeRuNuSeR3 --size large
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username FiLeRuNuSeR4 --size large
+# Create large thumbnails (previews) for files - this task only works per-user
+docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME1 --size large
+docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME2 --size large
+docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME3 --size large
+docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME4 --size large
+docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME5 --size large
+docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME5 --size large
 # Index content of files, extracting text, to allow searching within files - not recommended
 # usr/bin/docker exec -w /var/www/html/cron -it filerun php process_search_index_queue.php
 
@@ -46,28 +48,29 @@ docker exec -w /var/www/html/cron filerun php make_thumbs.php --username FiLeRuN
 # -----------------
 /usr/bin/bash ${SCRIPTDIR}/btrbk/btrbk-mail.sh
 # Perform monthtly maintenance on backup disk
-sudo run-if-today L zo && mount /mnt/disks/backup1
+sudo run-if-today L zo && mount /mnt/drives/backup1
 sudo run-if-today L zo && sleep 10
-sudo run-if-today L zo && btrfs balance start -dusage=10 -musage=5 /mnt/disks/backup1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo run-if-today L zo && btrfs balance start -v -dusage=20 -musage=10 /mnt/disks/backup1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
-sudo run-if-today L zo && btrfs scrub start -Bd -c 2 -n 4 /dev/sdb |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+# set the correct dev/sdX path for your backup1 drive
+sudo run-if-today L zo && btrfs scrub start -Bd -c 2 -n 4 /dev/sdc |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo run-if-today L zo && btrfs balance start -dusage=10 -musage=5 /mnt/drives/backup1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
+sudo run-if-today L zo && btrfs balance start -v -dusage=20 -musage=10 /mnt/drives/backup1 |& tee -a ${SCRIPTDIR}/logs/monthly.txt
 
 
+# Only if you still use Scrutiny
 # S.M.A.R.T. disk health scan on ALL disks (now that Backup1 is still spinning)
 # ----------------------
-mount /mnt/disks/backup1
-mount /mnt/disks/parity1
-sleep 10
-docker exec scrutiny /app/scrutiny-collector-metrics run
-sleep 10
-umount /mnt/disks/backup1
+#mount /mnt/drives/backup1
+#sleep 10
+#docker exec scrutiny /app/scrutiny-collector-metrics run
+#sleep 10
+#umount /mnt/disks/backup1
 
 
 # PARITY-BASED BACKUP (now that Parity1 is still spinning)
 # -------------------
-python3 ${SCRIPTDIR}/snapraid/snapraid-btrfs-runner.py -c ${SCRIPTDIR}/snapraid/snapraid-btrfs-runner.conf
-sleep 10
-umount /mnt/disks/parity1
+#python3 ${SCRIPTDIR}/snapraid/snapraid-btrfs-runner.py -c ${SCRIPTDIR}/snapraid/snapraid-btrfs-runner.conf
+#sleep 10
+#umount /mnt/disks/parity1
 
 
 # Delete temp file, follow up tasks can continue
