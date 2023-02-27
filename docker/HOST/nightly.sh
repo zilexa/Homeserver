bdc170483038c998086e412d950cc73d29082ab1
ZileXa <zilexa@gmx.com>
2023-02-27 22:21:28 +0100

    Update nightly.sh

--- docker/HOST/nightly.sh
+++ docker/HOST/nightly.sh
@@ -21,27 +21,10 @@ python3 ${SCRIPTDIR}/mediacleaner/media_cleaner.py |& tee -a ${SCRIPTDIR}/logs/m
 #/usr/bin/bash ${SCRIPTDIR}/archiver.sh /mnt/drives/cache/users /mnt/pool-nocache/users 30
 
 
-# FileRun 
-# -------
-# Empty trash >30 days old files
-docker exec -w /var/www/html/cron filerun php empty_trash.php -days 30
-# Clear db of files/folders that no longer exist
-docker exec -w /var/www/html/cron filerun php paths_cleanup.php --deep
-# Index filenames for files created outside FileRun
-docker exec -w /var/www/html/cron filerun php index_filenames.php /user-files true
-# Read metadata of files created outside FileRun, the UI adjusts to photos (GPS), videos etc and has specific options per filetype
-docker exec -w /var/www/html/cron filerun php metadata_index.php 
-# Create thumbnails for files
-docker exec -w /var/www/html/cron filerun php make_thumbs.php
-# Create large thumbnails (previews) for files - this task only works per-user
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME1 --size large
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME2 --size large
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME3 --size large
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME4 --size large
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME5 --size large
-docker exec -w /var/www/html/cron filerun php make_thumbs.php --username USERNAME5 --size large
-# Index content of files, extracting text, to allow searching within files - not recommended
-# usr/bin/docker exec -w /var/www/html/cron -it filerun php process_search_index_queue.php
+# FileRun - maintenance
+# ---------------------
+# Cleanup non-existing paths, cleanup >30 day old trash, index files created outside FileRun, read metadata  and generate thumbs of such files
+su ${USER} -c ${SCRIPTDIR}/filerun.sh
 
 
 # SUBVOLUMES BACKUP  
