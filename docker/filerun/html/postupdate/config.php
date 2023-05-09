c834b1bc14462672446def91b101a3961e470de5
ZileXa <zilexa@gmx.com>
2023-05-09 15:49:52 +0200

    Delete config.php

--- docker/filerun/html/postupdate/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$config['app']['ui']['enable_favicon_ico'] = true;
-$config['app']['weblinks']['audio_file_types'] = array("mp3", "ogg", "flac", "wav", "aac");
-$config['app']['metadata']['indexing']['web']['max_files'] = 25;
-$config['app']['filelog']['enable_for_shares'] = true;
