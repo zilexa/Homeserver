66adad58611eb53c3a9404051f89b6db2e867866
ZileXa <zilexa@gmx.com>
2023-05-31 13:25:04 +0200

    Update monthly.sh
    Attempt to fix docker update summary, fix bleachbit for user and general issue at the beginning of the file, not creating the monthly.tmp file.

--- docker/HOST/monthly.sh
+++ docker/HOST/monthly.sh
@@ -11,11 +11,10 @@ while [[ -f ${SCRIPTDIR}/running-tasks ]] ; do
    sleep 10 ;
 done
 
-# Delete old monthly file. 
-rm ${SCRIPTDIR}/logs/monthly.tmp
 
 # Create monthly email body, add title and current date
 # -----------------------------------------------------
+touch ${SCRIPTDIR}/logs/monthly.tmp
 echo -e "\n_____MONTHLY HOUSEKEEPING TASKS_____" > ${SCRIPTDIR}/logs/monthly.tmp
 
 
@@ -32,7 +31,7 @@ tail -n 4 ${SCRIPTDIR}/logs/bleachbit.tmp >> ${SCRIPTDIR}/logs/monthly.tmp
 rm ${SCRIPTDIR}/logs/bleachbit.tmp
 
 # Run Bleachbit for regular user to clean files in $HOME
-touch ${SCRIPTDIR}/logs/bleachbit.tmp
+su -l ${LOGUSER} -c 'touch ${SCRIPTDIR}/logs/bleachbit.tmp'
 su -l ${LOGUSER} -c 'bleachbit --preset --clean |& tee -a ${SCRIPTDIR}/logs/bleachbit.tmp'
 # Add the summary of Bleachbit output to our monthly mail
 echo -e "\nBLEACHBIT - user level cleanup (/home folder).. \n" >> ${SCRIPTDIR}/logs/monthly.tmp
@@ -69,39 +68,40 @@ echo -e "\nDocker updates finished. \n" >> ${SCRIPTDIR}/logs/monthly.tmp
 # -----------------------
 echo -e "\n____________FILESTYSTEMS____________\n" >> ${SCRIPTDIR}/logs/monthly.tmp
 echo -e "\n FILESTEM housekeeping.." >> ${SCRIPTDIR}/logs/monthly.tmp
-echo -e "\nMount drives that are by default unmounted..\n" >> ${SCRIPTDIR}/logs/monthly.tmp
-mount /mnt/drives/backup1 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
-mount /mnt/drives/data0 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
 echo -e "\nScrub btrfs filesystems..\n" >> ${SCRIPTDIR}/logs/monthly.tmp
 btrfs scrub start -Bd -c 2 -n 4 /dev/nvme0n1p2 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
-btrfs scrub start -Bd -c 2 -n 4 /dev/sda |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
-btrfs scrub start -Bd -c 2 -n 4 /dev/sdb |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
+btrfs scrub start -Bd -c 2 -n 4 /dev/sda1 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
+btrfs scrub start -Bd -c 2 -n 4 /dev/sdc |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
+mount /mnt/drives/backup1 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
+btrfs scrub start -Bd -c 2 -n 4 /dev/sdb1 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
 
 # Run btrfs balance monthly, first 10% data, then try 20%
 # -------------------------
 echo -e "\nRun BTRFS Balance using btrfs mail list recommendation\n" >> ${SCRIPTDIR}/logs/monthly.tmp
 btrfs balance start -dusage=85 / |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
 btrfs balance start -dusage=85 /mnt/drives/data0 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
+btrfs balance start -dusage=85 /mnt/drives/data1 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
 btrfs balance start -dusage=85 /mnt/drives/backup1 |& tee -a ${SCRIPTDIR}/logs/monthly.tmp
 
 
 # Storage Status Report
 # -----------------------------------------------------
-echo -e "\n\n_________STORAGE STATUS REPORT________" > ${SCRIPTDIR}/logs/storagereport.tmp
-echo -e "\n               ~~~ STORAGE PER USER ~~~" >> ${SCRIPTDIR}/logs/storagereport.tmp
-sudo btrfs filesystem du -s /mnt/pool/users/* >> ${SCRIPTDIR}/logs/storagereport.tmp
-echo -e "\n                ~~~ USERS & OS filesystem ~~~" >> ${SCRIPTDIR}/logs/storagereport.tmp
-sudo btrfs fi usage / | grep 'Free (estimated)' >> ${SCRIPTDIR}/logs/storagereport.tmp
-sudo df -h /dev/nvme0n1p2 >> ${SCRIPTDIR}/logs/storagereport.tmp
-echo -e "\n                ~~~ MEDIA filesystem ~~~" >> ${SCRIPTDIR}/logs/storagereport.tmp
-sudo btrfs fi usage /mnt/pool/media | grep 'Free (estimated)' >> ${SCRIPTDIR}/logs/storagereport.tmp
-sudo df -h /dev/sdb >> ${SCRIPTDIR}/logs/storagereport.tmp
-echo -e "\n               ~~~ BACKUPS filesystem ~~~" >> ${SCRIPTDIR}/logs/storagereport.tmp
-sudo btrfs fi usage /mnt/pool/backup1 | grep 'Free (estimated)' >> ${SCRIPTDIR}/logs/storagereport.tmp
-sudo df -h /dev/sda >> ${SCRIPTDIR}/logs/storagereport.tmp
-echo -e "\nUnmount drives..\n" >> ${SCRIPTDIR}/logs/monthly.tmp
-umount /mnt/drives/data0 |& tee -a ${SCRIPTDIR}/logs/storagereport.tmp
-umount /mnt/drives/backup1 |& tee -a ${SCRIPTDIR}/logs/storagereport.tmp
+echo -e "\n\n_________STORAGE STATUS REPORT________" > /tmp/storagereport.tmp
+echo -e "\n               ~~~ STORAGE PER USER ~~~" >> /tmp/storagereport.tmp
+sudo btrfs filesystem du -s /mnt/pool/users/* >> /tmp/storagereport.tmp
+echo -e "\n                ~~~ USERS filesystem ~~~" >> /tmp/storagereport.tmp
+sudo btrfs fi usage /mnt/pool/users | grep 'Free (estimated)' >> /tmp/storagereport.tmp
+sudo df -h /dev/sda1 >> /tmp/storagereport.tmp
+echo -e "\n                ~~~ MEDIA filesystem ~~~" >> /tmp/storagereport.tmp
+sudo btrfs fi usage /mnt/pool/media | grep 'Free (estimated)' >> /tmp/storagereport.tmp
+sudo df -h /dev/sdc >> /tmp/storagereport.tmp
+echo -e "\n               ~~~ BACKUPS filesystem ~~~" >> /tmp/storagereport.tmp
+sudo btrfs fi usage /mnt/pool/backup1 | grep 'Free (estimated)' >> /tmp/storagereport.tmp
+sudo df -h /dev/sdb1 >> /tmp/storagereport.tmp
+umount /mnt/drives/backup1 |& tee -a /tmp/storagereport.tmp
+echo -e "\n                   ~~~ OS filesystem ~~~" >> /tmp/storagereport.tmp
+sudo btrfs fi usage / | grep 'Free (estimated)' >> /tmp/storagereport.tmp
+sudo df -h /dev/nvme0n1p2 >> /tmp/storagereport.tmp
 
 
 # Update system
@@ -110,7 +110,7 @@ echo -e "\n____________SYSTEM UPDATE____________\n" >> ${SCRIPTDIR}/logs/monthly
 # Query mirrors servers (on this continent only) to ensure updates are downloaded via the fastest HTTPS server
 pacman-mirrors --continent --api -P https
 # Perform update, force refresh of update database files
-pamac update --force-refresh --no-confirm >> ${SCRIPTDIR}/logs/monthly.tmp
+pamac update --force-refresh --no-confirm
 # Clean packages cache
 pamac clean --keep 3 --no-confirm >> ${SCRIPTDIR}/logs/monthly.tmp
 # Check if a restart is needed, add this notification to the top of the email
@@ -126,3 +126,5 @@ rm /tmp/storagereport.tmp
 cat ${SCRIPTDIR}/logs/monthly.tmp >> /tmp/monthlymail.tmp
 mail -s "Obelix server - monthly report" asterix < /tmp/monthlymail.tmp
 rm /tmp/monthlymail.tmp
+# Delete old monthly file. 
+rm ${SCRIPTDIR}/logs/monthly.tmp
