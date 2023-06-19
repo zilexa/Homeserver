3303fd4adbfb9382f1cf0f941c8ad98bb00ae2c4
ZileXa <zilexa@gmx.com>
2023-06-19 20:19:28 +0200

    Delete linkfolders.sh

--- filesystem/folderstructure/linkfolders.sh
+++
@@ -1,5 +0,0 @@
-ln -s /mnt/pool/Users/Shared/Documents $HOME/Documents
-ln -s /mnt/pool/Users/Shared/Pictures $HOME/Pictures
-ln -s /mnt/pool/Users/Shared/Desktop $HOME/Desktop
-ln -s /mnt/pool/Media $HOME/Media
-ln -s /mnt/pool/Media/Music $HOME/Music
