b77ec4f2f2addfb30c071f29bcf2b657e557cc51
ZileXa <zilexa@gmx.com>
2025-02-06 21:19:12 +0100

    Update tempactions.sh

--- tempactions.sh
+++ tempactions.sh
@@ -7,27 +7,29 @@ sudo hostnamectl hostname obelix.o
 # Allow services running under current user to linger after logout
 ctl enable-linger $USER
 
-# Install required system apps
-rpm-ostree install podman-compose cockpit-system cockpit-ws cockpit-files cockpit-networkmanager cockpit-ostree cockpit-podman cockpit-selinux cockpit-storaged
-sudo systemctl enable --now cockpit.socket
-sudo firewall-cmd --add-service=cockpit
-sudo firewall-cmd --add-service=cockpit --permanent
-
-DO NOT USE ujust cockpit
-
-# Allow containers to access GPU directly for hardware acceleration (required for Jellyfin)
-sudo setsebool -P container_use_dri_devices 1
-
 # Add registry for docker images
+mkdir $HOME/.config/containers/
 ❯ cp /etc/containers/registries.conf $HOME/.config/containers/
 
-# Temp solution: lower priv ports. A better solution would be to use firewall rules - to figure out later. First want to get the containers up and running again
+# Lower privileged ports
 sudo nano /etc/sysctl.d/podman-privileged-ports.conf
 # add net.ipv4.ip_unprivileged_port_start=80 to this file
 # Apply change:
 sudo sysctl --load /etc/sysctl.d/podman-privileged-ports.conf
 
-# Create required network for cadyy
-podman network create net-caddy
+# Install required system apps
+rpm-ostree install podman-compose cockpit-system cockpit-ws cockpit-files cockpit-networkmanager cockpit-ostree cockpit-podman cockpit-selinux cockpit-storaged
+# REBOOT!
+# Configure cockpit
+sudo systemctl enable --now cockpit.socket
+sudo firewall-cmd --add-service=cockpit
+sudo firewall-cmd --add-service=cockpit --permanent
+sudo setsebool -P nis_enabled 1
+
+# Configure podman
+systemctl --user enable --now podman.socket
+## Enable GPU access for hardware accelerated video transcoding (for Jellyfin)
+sudo setsebool -P container_use_dri_devices 1
+
 
 # Thats it.. all other modifications should be in the compose file.. 
