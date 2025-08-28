b7a48f6cd0a72ea7fd552ba8bd9f0b647f745c3b
ZileXa <zilexa@gmx.com>
2025-08-28 21:51:02 +0200

    Update uBlue-homeserver.sh

--- uBlue-homeserver.sh
+++ uBlue-homeserver.sh
@@ -5,41 +5,61 @@
 
 Basics after a new laptop/pc install (Gnome based Linux): 
 
-1. Go to Settings > System > Region
+1. After setup is completed, dont do anything else but this first:
+https://github.com/zilexa/Bluefin-Gnome-Intuitive follow the instructions. 
+Reboot afterwards!
+
+After the reboot, do the bare basics:
+2. Go to Settings > System > Region
    > Change Formats to your region. 
-2. Settings > System > About 
+3. Settings > System > About 
    > Change your device name
-3. Settings > System > Remote Desktop
+4. Settings > System > Remote Desktop
    > Enable Desktop Sharing & Remote Control
    > Set a password you remember
-4. Settings System > SSH
+5. Settings System > SSH
    > Enable SSH
-5. Settings > Power 
+6. Settings > Power 
    > Disable Suspend when plugged in if this is a device that will be running 24x7
-6. Close Settings and go to Apps > Extensions
+
+Now set up Tailscale, make sure you are logged into Tailscale in the browser already (or create an account, just use Google to login). 
+7. Go to Apps > Extensions
    > Find TailscaleQS
    > Hit the tiny arrow at the right to see its description.
    > Note it says to run a command first.
    > Open Terminal app, type over that command and run it. 
-7. > Now you can run `tailscale up` and it will give you an URL to login. When done:
+8. > Now you can run `tailscale up` and it will give you an URL to login. When done:
    > Go to the system tray, hit the arrow next to Tailscale > Settings. 
    > Enable "Allow LAN Access". The rest disabled. 
 
-8. Tailscale Exit Node: If this is your homeserver/nas that will be running 24x7, you can use it as a Tailscale Exit Node, and optionally allow other devices to tunnel
+9. Tailscale Exit Node: If this is your homeserver/nas that will be running 24x7, you can use it as a Tailscale Exit Node, and optionally allow other devices to tunnel
    their traffic through this server (for example: to share Netflix). This turns your system into a VPN Tunnel endpoint (like a paid VPN service does). 
    This is NOT needed for other devices to access services running on this server. But its a nice to have option:
    > run: tailscale set --advertise-exit-node
    > go on the web to tailscale.com and login to admin console, there you can Approve your device as exit node. 
    > You need to also run: tailscale set --advertise-routes=192.168.1.0/24
    > and probably accept this in the webUI. 
-   > Now go to the system tray, make sure "Allow LAN Access" and "Accept Routes" are enabled. Make sure "Allow DNS" is disabled since you don't need adfiltering. 
+   > Now go to the system tray, make sure "Allow LAN Access" and "Accept Routes" are enabled. Make sure "Allow DNS" is disabled since you dont need adfiltering. 
    Note: "Accept Routes" only needs to be enabled if you set this device up as Exit Node. Otherwise leave it disabled.
-   
 
-sudo hostnamectl hostname obelix.o
+Extra handy:
+10. Allow services running under current user to linger after logout:
+   > loginctl enable-linger $USER
 
-# Allow services running under current user to linger after logout
-ctl enable-linger $USER
+Docker like you are used to: 
+11. Setup docker: 
+   > ujust dx-group 
+   If this is incorrect just run "ujust", the list of scripts will be shown, find the one with the description saying it sets up docker.
+
+SELinux:
+12. Change SELinux to permissive mode (you will get warnings but wont be blocked)
+> sudo nano /etc/selinux/config
+> change to SELINUX=permissive
+
+Save changes and reboot. 
+
+## Enable GPU access for hardware accelerated video transcoding (for Jellyfin) Probalby not neccessary:
+sudo setsebool -P container_use_dri_devices 1
 
 # Add registry for docker images
 ##mkdir $HOME/.config/containers/
@@ -51,7 +71,6 @@ ctl enable-linger $USER
 # Apply change:
 ##sudo sysctl --load /etc/sysctl.d/podman-privileged-ports.conf
 
-Instead of the above, use the Bluefin-provided method to setup Docker: ujust dx-group
 
 # To manage containers via Cockpit, note this is optional and should NOT be used! Just use Podman Desktop isntead!!
 ## rpm-ostree install cockpit-system cockpit-ws cockpit-files cockpit-networkmanager cockpit-ostree cockpit-podman cockpit-selinux cockpit-storaged
@@ -64,11 +83,6 @@ Instead of the above, use the Bluefin-provided method to setup Docker: ujust dx-
 
 # Configure podman
 ##systemctl --user enable --now podman.socket
-## Enable GPU access for hardware accelerated video transcoding (for Jellyfin)
-sudo setsebool -P container_use_dri_devices 1
 
-# Change SELinux to permissive mode (or disable), permissive mode will show warnings, not block anything:
-sudo nano /etc/selinux/config
-change to SELINUX=permissive
-Save changes and reboot. 
-# Thats it.. all other modifications should be in the compose file.. 
+
+
